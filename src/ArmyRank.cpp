f07767fc373cbff4bb26ba7cef58e39e96605a60
zxdhe121@163.com <zxdhe121@163.com>
2019-12-06 12:21:12 +0800

    升级军衔时加入视觉效果

--- src/ArmyRank.cpp
+++ src/ArmyRank.cpp
@@ -1,4 +1,4 @@
-// army rank engine
+﻿// army rank engine
 
 #include "ArmyRank.h"
 #include "chat.h"
@@ -67,51 +67,51 @@ ArmyRank::ArmyRank() { }
 
 ArmyRank::~ArmyRank()
 {
-	for (std::unordered_map<uint64, uint8>::iterator itr = PlayerRank.begin(); itr != PlayerRank.end(); ++itr)
-		delete &itr->second;
-	for (std::unordered_map<uint32, uint8>::iterator itr = ItemRank.begin(); itr != ItemRank.end(); ++itr)
-		delete &itr->second;
+    for (std::unordered_map<uint64, uint8>::iterator itr = PlayerRank.begin(); itr != PlayerRank.end(); ++itr)
+        delete& itr->second;
+    for (std::unordered_map<uint32, uint8>::iterator itr = ItemRank.begin(); itr != ItemRank.end(); ++itr)
+        delete& itr->second;
 
-	PlayerRank.clear();
-	ItemRank.clear();
+    PlayerRank.clear();
+    ItemRank.clear();
 }
 
 ArmyRank* ArmyRank::instance()
 {
-	static ArmyRank instance;
-	return &instance;
+    static ArmyRank instance;
+    return &instance;
 }
 
 class ArmyRank_Load_Config : public WorldScript
 {
 public: ArmyRank_Load_Config() : WorldScript("ArmyRank_Load_Config") { };
 
-		void OnAfterConfigLoad(bool /*reload*/)
-		{
+        void OnAfterConfigLoad(bool /*reload*/)
+        {
             sLog->outString(" ArmyRank Engine load config");
-			
-			QueryResult itemQuery = WorldDatabase.PQuery("SELECT entry, army_rank FROM item_template;");
-			if (itemQuery)
-			{
-				do
-				{
-					Field* fields = itemQuery->Fetch();
-					uint32 item_id = fields[0].GetUInt32();
-					uint8 rank = fields[1].GetUInt8();
 
-					// Save the DB values to the MyData object
-					sArmyRank->SetItemRank(item_id, rank);
+            QueryResult itemQuery = WorldDatabase.PQuery("SELECT entry, army_rank FROM item_template;");
+            if (itemQuery)
+            {
+                do
+                {
+                    Field* fields = itemQuery->Fetch();
+                    uint32 item_id = fields[0].GetUInt32();
+                    uint8 rank = fields[1].GetUInt8();
 
-				} while (itemQuery->NextRow());
-			}
-			sLog->outString(" ArmyRank Engine config loaded");
-		}
+                    // Save the DB values to the MyData object
+                    sArmyRank->SetItemRank(item_id, rank);
+
+                } while (itemQuery->NextRow());
+            }
+            sLog->outString(" ArmyRank Engine config loaded");
+        }
 };
 
 void ArmyRank::SetPlayerRank(uint64 player_guid, uint8 rank, bool update_DB)
 {
-	sArmyRank->PlayerRank[player_guid] = rank;
-	if(update_DB){
+    sArmyRank->PlayerRank[player_guid] = rank;
+    if (update_DB) {
         CharacterDatabase.PExecute("UPDATE characters SET army_rank = %u WHERE guid = %u ;", sArmyRank->PlayerRank[player_guid], player_guid);
     }
 
@@ -121,20 +121,20 @@ class ArmyRank_Player_Script : public PlayerScript
 {
 public: ArmyRank_Player_Script() : PlayerScript("ArmyRank_Player_Script") { };
 
-		void OnLoadFromDB(Player* player)
-		{
-		    uint64 playerGUID = player->GetGUID();
-			QueryResult PlayerQuery = CharacterDatabase.PQuery("SELECT army_rank FROM characters WHERE guid = %u ;", playerGUID);
+        void OnLoadFromDB(Player* player)
+        {
+            uint64 playerGUID = player->GetGUID();
+            QueryResult PlayerQuery = CharacterDatabase.PQuery("SELECT army_rank FROM characters WHERE guid = %u ;", playerGUID);
             Field* fields = PlayerQuery->Fetch();
             uint8 rank = fields[0].GetUInt8();
             // Save the DB values to the MyData object
             sArmyRank->SetPlayerRank(playerGUID, rank, false);
 
             //bonus talent points
-            if(rank && rank > 0){
+            if (rank && rank > 0) {
                 player->RewardOtherBonusTalentPoints(rank);
             }
-		}
+        }
 
         bool OnBeforeBuyItemFromVendor(Player* player, uint64 /*vendorguid*/, uint32 /*vendorslot*/, uint32& item, uint8 /*count*/, uint8 /*bag*/, uint8 /*slot*/) {
             uint8 playerRank = sArmyRank->GetPlayerRank(player->GetGUID());
@@ -152,40 +152,40 @@ class ArmyRank_Info_Show_Script : public ItemScript
 {
 public: ArmyRank_Info_Show_Script() : ItemScript("ArmyRank_Info_Show_Script") { };
 
-		bool OnUse(Player* player, Item* item, SpellCastTargets const& targets)
-		{
+        bool OnUse(Player* player, Item* item, SpellCastTargets const& targets)
+        {
             uint64 playerGUID = player->GetGUID();
-			uint8 rank = sArmyRank->GetPlayerRank(playerGUID);
+            uint8 rank = sArmyRank->GetPlayerRank(playerGUID);
 
-			ChatHandler(player->GetSession()).PSendSysMessage("**********************************");
-			ChatHandler(player->GetSession()).PSendSysMessage(LANG_ARMY_RANK_INFO, (player->GetSession()->GetTrinityString(ARMY_RANK_NAME_INDEX + rank)), rank);
-			ChatHandler(player->GetSession()).PSendSysMessage("**********************************");
-			return true;
-		}
+            ChatHandler(player->GetSession()).PSendSysMessage("**********************************");
+            ChatHandler(player->GetSession()).PSendSysMessage(LANG_ARMY_RANK_INFO, (player->GetSession()->GetTrinityString(ARMY_RANK_NAME_INDEX + rank)), rank);
+            ChatHandler(player->GetSession()).PSendSysMessage("**********************************");
+            return true;
+        }
 };
 
 class ArmyRank_Up_Script : public ItemScript
 {
 public: ArmyRank_Up_Script() : ItemScript("ArmyRank_Up_Script") { };
 
-		bool OnUse(Player* player, Item* item, SpellCastTargets const& targets)
-		{
+        bool OnUse(Player* player, Item* item, SpellCastTargets const& targets)
+        {
             uint64 playerGUID = player->GetGUID();
-			uint8 playerRank = sArmyRank->GetPlayerRank(playerGUID);
+            uint8 playerRank = sArmyRank->GetPlayerRank(playerGUID);
             uint8 itemRank = sArmyRank->GetItemRank(item->GetEntry());
             if (itemRank != playerRank) {
                 player->GetSession()->SendAreaTriggerMessage(player->GetSession()->GetTrinityString(LANG_ARMY_RANK_USE_ITEM_ERROR), playerRank, itemRank);
                 return false;
             }
-			sArmyRank->SetPlayerRank(playerGUID, playerRank + 1, true);
+            sArmyRank->SetPlayerRank(playerGUID, playerRank + 1, true);
 
             player->RewardOtherBonusTalentPoints(1);
             player->InitTalentForLevel();
-			player->DestroyItemCount(item->GetEntry(), 1, true);
-
-			ChatHandler(player->GetSession()).PSendSysMessage(LANG_ARMY_RANK_LEVEL_UP, (player->GetSession()->GetTrinityString(ARMY_RANK_NAME_INDEX + playerRank + 1)), (playerRank + 1));
-			return true;
-		}
+            player->DestroyItemCount(item->GetEntry(), 1, true);
+            player->CastSpell(player, 31726);//视觉效果
+            ChatHandler(player->GetSession()).PSendSysMessage(LANG_ARMY_RANK_LEVEL_UP, (player->GetSession()->GetTrinityString(ARMY_RANK_NAME_INDEX + playerRank + 1)), (playerRank + 1));
+            return true;
+        }
 };
 
 
@@ -202,13 +202,28 @@ public:
 
     void sendMainMenu(Player* player, Creature* creature)
     {
-        player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_RANK_VENDOR_OPTION_S1), GOSSIP_SENDER_MAIN, 1);
-        player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_RANK_VENDOR_OPTION_S2), GOSSIP_SENDER_MAIN, 2);
-        player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_RANK_VENDOR_OPTION_S3), GOSSIP_SENDER_MAIN, 3);
-        player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_RANK_VENDOR_OPTION_S4), GOSSIP_SENDER_MAIN, 4);
-        player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_RANK_VENDOR_OPTION_VETERAN), GOSSIP_SENDER_MAIN, 5);
-        player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_RANK_VENDOR_OPTION_VINDICATOR), GOSSIP_SENDER_MAIN, 6);
-        player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_RANK_VENDOR_OPTION_GUARDIAN), GOSSIP_SENDER_MAIN, 7);
+        uint8 arenaSeason = sWorld->getIntConfig(CONFIG_ARENA_SEASON_ID);
+        if (arenaSeason >= 1) {
+            player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_RANK_VENDOR_OPTION_S1), GOSSIP_SENDER_MAIN, 1);
+        }
+        if (arenaSeason >= 2) {
+            player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_RANK_VENDOR_OPTION_S2), GOSSIP_SENDER_MAIN, 2);
+        }
+        if (arenaSeason >= 3) {
+            player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_RANK_VENDOR_OPTION_S3), GOSSIP_SENDER_MAIN, 3);
+        }
+        if (arenaSeason >= 4) {
+            player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_RANK_VENDOR_OPTION_S4), GOSSIP_SENDER_MAIN, 4);
+        }
+        if (arenaSeason >= 1) {
+            player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_RANK_VENDOR_OPTION_VETERAN), GOSSIP_SENDER_MAIN, 5);
+        }
+        if (arenaSeason >= 3) {
+            player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_RANK_VENDOR_OPTION_VINDICATOR), GOSSIP_SENDER_MAIN, 6);
+        }
+        if (arenaSeason >= 4) {
+            player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_RANK_VENDOR_OPTION_GUARDIAN), GOSSIP_SENDER_MAIN, 7);
+        }
         player->ADD_GOSSIP_ITEM(0, player->GetSession()->GetTrinityString(LANG_ARMY_RANK_VENDOR_OPTION_CLOSE), GOSSIP_SENDER_MAIN, 99);
         player->SEND_GOSSIP_MENU(1, creature->GetGUID());
     }
@@ -268,10 +283,19 @@ public:
 
     void sendMainMenu(Player* player, Creature* creature)
     {
-        player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_HEROIC_VENDOR_OPTION_S1), GOSSIP_SENDER_MAIN, 1);
-        player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_HEROIC_VENDOR_OPTION_S2), GOSSIP_SENDER_MAIN, 2);
-        player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_HEROIC_VENDOR_OPTION_S3), GOSSIP_SENDER_MAIN, 3);
-        player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_HEROIC_VENDOR_OPTION_S4), GOSSIP_SENDER_MAIN, 4);
+        uint8 arenaSeason = sWorld->getIntConfig(CONFIG_ARENA_SEASON_ID);
+        if (arenaSeason >= 1) {
+            player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_HEROIC_VENDOR_OPTION_S1), GOSSIP_SENDER_MAIN, 1);
+        }
+        if (arenaSeason >= 2) {
+            player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_HEROIC_VENDOR_OPTION_S2), GOSSIP_SENDER_MAIN, 2);
+        }
+        if (arenaSeason >= 3) {
+            player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_HEROIC_VENDOR_OPTION_S3), GOSSIP_SENDER_MAIN, 3);
+        }
+        if (arenaSeason >= 4) {
+            player->ADD_GOSSIP_ITEM(1, player->GetSession()->GetTrinityString(LANG_ARMY_HEROIC_VENDOR_OPTION_S4), GOSSIP_SENDER_MAIN, 4);
+        }
         //player->ADD_GOSSIP_ITEM(10, player->GetSession()->GetTrinityString(LANG_ARMY_HEROIC_VENDOR_OPTION_VETERAN), GOSSIP_SENDER_MAIN, 5);
         //player->ADD_GOSSIP_ITEM(10, player->GetSession()->GetTrinityString(LANG_ARMY_HEROIC_VENDOR_OPTION_VINDICATOR), GOSSIP_SENDER_MAIN, 6);
         //player->ADD_GOSSIP_ITEM(10, player->GetSession()->GetTrinityString(LANG_ARMY_HEROIC_VENDOR_OPTION_GUARDIAN), GOSSIP_SENDER_MAIN, 7);
@@ -373,10 +397,10 @@ public:
 
 void AddSC_ArmyRank()
 {
-	new ArmyRank_Load_Config;
-	new ArmyRank_Player_Script;
-	new ArmyRank_Info_Show_Script;
-	new ArmyRank_Up_Script;
+    new ArmyRank_Load_Config;
+    new ArmyRank_Player_Script;
+    new ArmyRank_Info_Show_Script;
+    new ArmyRank_Up_Script;
     new ArmyRank_season_vendor_Script;
     new ArmyRank_season_vendor_heroic_Script;
     new ArmyRank_vendor_others_Script;
